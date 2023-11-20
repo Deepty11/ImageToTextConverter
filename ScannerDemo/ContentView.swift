77afaaf95fc041c3e5cf3a56b4db1c9be49c22e2
Rehnuma Reza (Deepty) <rehnuma.deepty@therapservices.net>
2023-11-20 15:14:55 +0600

    Initial Commit

---
+++ ScannerDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ScannerDemo
+//
+//  Created by Rehnuma Reza Deepty on 20/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ScannerDemo/ScannerDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScannerDemoApp.swift
+//  ScannerDemo
+//
+//  Created by Rehnuma Reza Deepty on 20/11/23.
+//
+
+import SwiftUI
+
+@main
+struct ScannerDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
